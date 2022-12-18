4a0cee91e0f85df20fcf484e127e3b4eebf97eb6
Caltinor <62700786+Caltinor@users.noreply.github.com>
2022-12-18 06:18:20 -0500

    reformat query strings

--- Database.py
+++ Database.py
@@ -11,7 +11,7 @@ cursor.executescript(open('table_definitions.sql', 'r').read())
 print("Tables constructed if absent")
 
 def get_info(modID :str, keyword :str, filter :str):
-    sql = f'SELECT message_body, mod_version FROM info WHERE cmdroot = "{modID}" AND keyword = "{keyword}" AND mod_version LIKE "%{filter}%";'
+    sql = f"SELECT message_body, mod_version FROM info WHERE cmdroot = '{modID}' AND keyword = '{keyword}' AND mod_version LIKE '%{filter}%';"
     return cursor.execute(sql).fetchall()
 
 def get_info_formatted(modID :str, keyword :str, filter :str):
@@ -24,15 +24,15 @@ def get_info_formatted(modID :str, keyword :str, filter :str):
     return formatted_lines if len(formatted_lines) > 0 else [discord.Embed(title='This keyword has no associated documentation')]
 
 def set_info(modID :str, keyword :str, version :str, message :str):
-    insert_sql = f'INSERT INTO info (cmdroot, keyword, mod_version, message_body) VALUES("{modID}","{keyword}","{version}","{message}");'
-    update_sql = f'UPDATE info SET message_body = "{message}" WHERE cmdroot = "{modID}" AND keyword = "{keyword}" AND mod_version = "{version}";'
+    insert_sql = f"INSERT INTO info (cmdroot, keyword, mod_version, message_body) VALUES('{modID}','{keyword}','{version}','{message}');"
+    update_sql = f"UPDATE info SET message_body = '{message}' WHERE cmdroot = '{modID}' AND keyword = '{keyword}' AND mod_version = '{version}';"
     results = get_info(modID=modID, keyword=keyword, filter=version)
     final_sql = update_sql if len(results) > 0 else insert_sql
     connection.execute(final_sql)
     connection.commit()
 
 def get_compat(mod_a :str, mod_b :str, filter :str):
-    sql = f'SELECT message_body, mod_a_version FROM compat WHERE mod_a = "{mod_a}" AND mod_b = "{mod_b}" AND mod_a_version LIKE "%{filter}%";'
+    sql = f"SELECT message_body, mod_a_version FROM compat WHERE mod_a = '{mod_a}' AND mod_b = '{mod_b}' AND mod_a_version LIKE '%{filter}%';"
     return cursor.execute(sql).fetchall()    
 
 def get_compat_formatted(mod_a :str, mod_b :str, filter :str):
@@ -44,8 +44,8 @@ def get_compat_formatted(mod_a :str, mod_b :str, filter :str):
     return formatted_lines if len(formatted_lines) > 0 else [discord.Embed(title='No incompatibilities have been reported for these mods and version.')]
 
 def set_compat(mod_a :str, mod_b :str, version :str, message :str):
-    insert_sql = f'INSERT INTO compat (mod_a, mod_b, message_body, mod_a_version) VALUES("{mod_a}","{mod_b}","{message}","{version}");'
-    update_sql = f'UPDATE compat SET message_body = "{message}" WHERE mod_a = "{mod_a}" AND mod_b = "{mod_b}" AND mod_a_version = "{version}";'
+    insert_sql = f"INSERT INTO compat (mod_a, mod_b, message_body, mod_a_version) VALUES('{mod_a}','{mod_b}','{message}','{version}');"
+    update_sql = f"UPDATE compat SET message_body = '{message}' WHERE mod_a = '{mod_a}' AND mod_b = '{mod_b}' AND mod_a_version = '{version}';"
     results = get_compat(mod_a=mod_a, mod_b=mod_b, filter=version)
     final_sql = update_sql if len(results) > 0 else insert_sql
     connection.execute(final_sql)
